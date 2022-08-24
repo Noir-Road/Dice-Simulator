67e1f2d5d10c233e22c3df1233381f6b7d72ec53
Angel Ramirez <manuel.delgado@verizonconnect.com>
2022-08-24 01:14:45 -0500

    Se ajustan mejor los colliders y se agrega efecto de desvanecimiento a los numeros

--- Assets/Scripts/Toon Dice/Ground.cs
+++ Assets/Scripts/Toon Dice/Ground.cs
@@ -6,12 +6,14 @@ public class Ground : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
     [SerializeField] GameObject number6;
-    [SerializeField] GameObject number3;
-    [SerializeField] GameObject number4;
     [SerializeField] GameObject number5;
+    [SerializeField] GameObject number4;
+    [SerializeField] GameObject number3;
     [SerializeField] GameObject number2;
     [SerializeField] GameObject number1;
     public Transform dice;
+
+    public short numberSide = 0;
 
      public void OnTriggerStay(Collider collision)
      {
@@ -19,31 +21,38 @@ public class Ground : MonoBehaviour
         switch (_name)
             {
                  case "C1"://rutina 1
-                     Debug.Log("esto es un obje" + name);
+                     Debug.Log("Collider: " + _name + "Side: 1");
                      Instantiate(number6, dice.transform.position + new Vector3(0f, 5f, 0f), Quaternion.identity);
+                     numberSide = 1;
                      break;
                  case "C2"://rutina 2
-                     Debug.Log("esto es un objeto" + name);
+                     Debug.Log("Collider: " + _name + "Side: 2");
                      Instantiate(number5, dice.transform.position + new Vector3(0f, 5f, 0f), Quaternion.identity);
+                     numberSide = 2;
                      break;
                  case "C3"://rutina 3
-                     Debug.Log("esto es un objeto" + name);
+                     Debug.Log("Collider: " + _name + "Side: 3");
                      Instantiate(number4, dice.transform.position + new Vector3(0f, 5f, 0f), Quaternion.identity);
+                     numberSide = 3;
                      break;
                  case "C4"://rutina 4
-                     Debug.Log("esto es un objeto" + name);
+                     Debug.Log("Collider: " + _name + "Side: 4");
                      Instantiate(number3, dice.transform.position + new Vector3(0f, 5f, 0f), Quaternion.identity);
+                     numberSide = 4;
                      break;
                  case "C5"://rutina 5
-                     Debug.Log("esto es un objeto" + name);
+                     Debug.Log("Collider: " + _name + "Side: 5");
                      Instantiate(number2, dice.transform.position + new Vector3(0f, 5f, 0f), Quaternion.identity);
+                     numberSide = 5;
                      break;
                  case "C6"://rutina 6
-                     Debug.Log("esto es un objeto" + name);
+                     Debug.Log("Collider: " + _name + "Side: 6");
                      Instantiate(number1, dice.transform.position + new Vector3(0f, 5f, 0f), Quaternion.identity);
+                     numberSide = 6;
                      break;
                  default:
-                     Debug.Log("esto es un objeto default" + name);
+                     Debug.Log("No valid collider:" + _name);
+                     numberSide = -1;
                      break;
             }
      }
--- Assets/Scripts/Toon Dice/Numbers.cs
+++ Assets/Scripts/Toon Dice/Numbers.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Numbers : MonoBehaviour
 {
 
-    public float destroytime = 1.5f;
+    public float destroytime = 2.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+         float elapsedTime = 0;
+         Color startingColor = transform.GetComponent<Renderer>().material.color;
+         Color finalColor = new Color(startingColor.r, startingColor.g, startingColor.b, 0);
+         while (elapsedTime < destroytime)
+         {
+             transform.GetComponent<Renderer>().material.color = Color.Lerp(startingColor, finalColor, (elapsedTime / destroytime));
+             elapsedTime += Time.deltaTime;
+             //yield return null;
+         }
         Destroy(gameObject,destroytime);
 
 
